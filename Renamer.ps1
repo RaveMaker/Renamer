b0175e347729b0ff1f4821da6b4bcfd589045905
RaveMaker <ravemaker@gmail.com>
2019-07-08 13:30:10 +0300

    better testing

--- Renamer.ps1
+++ Renamer.ps1
@@ -6,6 +6,12 @@ Joins a computer to domain.
 The Renamer.ps1 script updates the computer account name
 and joins it to the specified domain.
 
+- Get Current IP Address
+- Calculate New Computer Name
+- Get current Computer Name and rename if needed including reboot
+- Check if Local Computer is Domain joined
+- Check if Object is in AD, if not then join in specific ou else just join and reboot
+
 .PARAMETER InputPath
 Specifies the path to the CSV-based input file.
 
@@ -19,27 +25,32 @@ None. Renamer.ps1 does not generate any output.
 PS> .\Renamer.ps1
 #>
 
-# Set-ExecutionPolicy RemoteSigned
-Set-ExecutionPolicy -Scope process -ExecutionPolicy allsigned
+# Set-ExecutionPolicy –ExecutionPolicy RemoteSigned
+
+# Dry run only $true/$false
+$dryRun = $true
+
+# Actions
+$joinDomain = $false
+$joinDomainLocally = $false
 
 # Declarations
-$destOU = "OU=Computers,OU=Physics,OU=ESC,DC=ad,DC=biu,DC=ac,DC=il"
 $domain = "ad.biu.ac.il"
 $shortDomain = "CCDOM"
+$destOU = "OU=Computers,OU=Physics,OU=ESC,DC=ad,DC=biu,DC=ac,DC=il"
 $department = "PH"
 
-$fullIPAddress = ((Test-Connection -ComputerName $env:ComputerName -Count 1).IPV4Address.IPAddressToString -split ('\.'))
-$network = [int]$fullIPAddress[1]
-$vlan = "{0:000}" -f [int]$fullIPAddress[2]
-$ipAddress = "{0:000}" -f [int]$fullIPAddress[3]
+# Network Params
+$fullIPAddress = ((Test-Connection -ComputerName $env:ComputerName -Count 1).IPV4Address.IPAddressToString)
+$splitPAddress = ($fullIPAddress -split ('\.'))
+$network = [int]$splitPAddress[1]
+$vlan = "{0:000}" -f [int]$splitPAddress[2]
+$ipAddress = "{0:000}" -f [int]$splitPAddress[3]
 
-$currentComputerName = gc env:computername
-$newCompName = $department + $vlan + $ipAddress
-
-# Check if computer has a BIU network address: 132.71.*.*
+# Check if computer has a valid network address: *.71.*.*
 if ($network -eq 70)
 {
-    Write-Host -ForegroundColor Green "Network ready"
+    Write-Host -ForegroundColor Green "Network ready $fullIPAddress"
 }
 else
 {
@@ -47,46 +58,72 @@ else
     exit 1
 }
 
-$credential = New-Object System.Management.Automation.PsCredential("[DOMAIN\JoinUser]", (ConvertTo-SecureString "[password]" -AsPlainText -Force))
-$domaininfo = New-Object DirectoryServices.DirectoryEntry(LDAP://[DOMAIN_CONTROLLER_IP_ADDRESS]/[domain root path e.g. dc = mydomain, dc = local], "[DOMAIN\ReadOnlyUser]", "[password]")
-$ComputerName = gc env:computername
-$searcher = New-Object System.DirectoryServices.DirectorySearcher($domaininfo)
-$searcher.filter = "(cn=$ComputerName)"
-$searchparm = $searcher.FindOne()
+# Computer name
+$currentComputerName = Get-Content env:computername
+$newCompName = $department + $vlan + $ipAddress
 
-$joinDomain = $true
-$renameComp = $true
-
-# Check if rename is needed
-if ($newCompName -eq $currentComputerName)
+# Rename Computer if needed
+if ($newCompName -ne $currentComputerName)
 {
-    $renameComp = $false
+    Write-Host -ForegroundColor Green "Changing $currentComputerName to $newCompName"
+    if (!($dryRun)) {
+        Rename-Computer -NewName $newCompName -Restart -ErrorAction Stop
+    }
+
 }
 
-# Check if computer is already joined to domain
+# AD Search params
+$credential = Get-Credential($shortDomain + "\")
+$domaininfo = New-Object DirectoryServices.DirectoryEntry("LDAP://ad.biu.ac.il/DC=ad,DC=biu,DC=ac,DC=il", $credential.UserName, $credential.Password)
+$searcher = New-Object System.DirectoryServices.DirectorySearcher($domaininfo)
+$searcher.filter = "(cn=$newCompName)"
+$searchparm = $searcher.FindOne
+
+# Check if computer is already joined to domain (locally)
 if ((Get-WmiObject win32_computersystem).PartOfDomain)
 {
     Write-Host -ForegroundColor Green "Computer is already joined to domain"
-    # Check if a Computer account object already exist in AD
-    $joinDomain = $false
+    $joinDomainLocally = $false
 }
 else
 {
     Write-Host -ForegroundColor Green "Computer is in a Workgroup!"
+    $joinDomainLocally = $true
 }
 
-if ($joinDomain)
+# Check if a Computer account object already exist in AD
+if (!($searchparm))
+{
+    Write-Host -ForegroundColor Green "Computer account not found in AD"
+    $joinDomain = $true
+}
+else
+{
+    Write-Host -ForegroundColor Green "Computer account found in AD"
+    $joinDomain = $false
+}
+
+# Join Domain
+if (($joinDomain) -or ($joinDomainLocally))
 {
     try
     {
-        Write-Host -ForegroundColor Green "Trying to join computer $newCompName to domain $domain"
-        if (!($searchparm))
+        # Check if a Computer account object already exist in AD
+        if ($joinDomain)
         {
-            #            Add-Computer -DomainName $domain -OUPath $destOU -Credential $credential $shortDomain\ -Restart -ErrorAction Stop
+            Write-Host -ForegroundColor Green "Trying to join computer $newCompName to domain $domain"
+            if (!($dryRun))
+            {
+                Add-Computer -DomainName $domain -OUPath $destOU -Credential $credential $shortDomain\ -Restart -ErrorAction Stop
+            }
         }
         else
         {
-            #            Add-Computer -DomainName $domain -Credential $credential $shortDomain\ -Restart -ErrorAction Stop
+            Write-Host -ForegroundColor Green "Trying to overwrite computer $newCompName in domain $domain"
+            if (!($dryRun))
+            {
+                Add-Computer -DomainName $domain -Credential $credential $shortDomain\ -Restart -ErrorAction Stop
+            }
         }
     }
     catch
