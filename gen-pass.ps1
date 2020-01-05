64e27f14a3e3871606f9b08918f611dc238355b3
RaveMaker <RaveMaker@gmail.com>
2020-01-05 15:01:21 +0200

    changed get-cred method

--- gen-pass.ps1
+++ gen-pass.ps1
@@ -1,3 +1,23 @@
+<#
+.SYNOPSIS
+Saves Credentials to file
+
+.DESCRIPTION
+The gen-pass.ps1 script saves get-cred to a file.
+
+.PARAMETER InputPath
+Specifies the path to the CSV-based input file.
+
+.INPUTS
+None. You cannot pipe objects to Renamer.ps1.
+
+.OUTPUTS
+None. Renamer.ps1 does not generate any output.
+
+.EXAMPLE
+PS> .\gen-pass.ps1
+#>
+
 $user = "myUser"
 $server = "myServer"
 $file = "c:\Windows\Renamer\password.txt"
