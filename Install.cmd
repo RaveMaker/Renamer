a6a8881187aa4725741f2dbf6cc1f0ee2772bad0
RaveMaker <RaveMaker@gmail.com>
2020-01-05 13:05:01 +0200

    silenced registry import

--- Install.cmd
+++ Install.cmd
@@ -1,5 +1,5 @@
 @echo off
 whoami /groups | find "12288" || echo "Run me as Admin" && pause && exit
 c:\Windows\Renamer\instsrv.exe Renamer c:\Windows\Renamer\srvany.exe
-c:\Windows\Renamer\renamer.reg
+regdit /s c:\Windows\Renamer\renamer.reg
 powershell Set-ExecutionPolicy Bypass
