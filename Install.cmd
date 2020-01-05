6da9b0a8cfd669468896c15f986d7323d6c37d97
RaveMaker <RaveMaker@gmail.com>
2020-01-05 13:01:39 +0200

    bypass execution policy

--- Install.cmd
+++ Install.cmd
@@ -1,5 +1,5 @@
 @echo off
-echo "Run me as Admin"
+whoami /groups | find "12288" || echo "Run me as Admin" && pause && exit
 c:\Windows\Renamer\instsrv.exe Renamer c:\Windows\Renamer\srvany.exe
 c:\Windows\Renamer\renamer.reg
-powershell Unblock-File c:\Windows\Renamer\Renamer.ps1
+powershell Set-ExecutionPolicy Bypass
