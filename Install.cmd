04d5e76278da0c03d9c057da1a048c8a21ba2357
RaveMaker <RaveMaker@gmail.com>
2020-01-05 12:39:04 +0200

    Unblock script

--- Install.cmd
+++ Install.cmd
@@ -2,4 +2,4 @@
 echo "Run me as Admin"
 c:\Windows\Renamer\instsrv.exe Renamer c:\Windows\Renamer\srvany.exe
 c:\Windows\Renamer\renamer.reg
-powershell Set-ExecutionPolicy RemoteSigned
+powershell Unblock-File c:\Windows\Renamer\Renamer.ps1
