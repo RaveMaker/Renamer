3d183864cb3b1e558f86918cd76e7966384597a0
RaveMaker <ravemaker@gmail.com>
2020-02-09 20:58:08 +0200

    fixed sc.exe path

--- Install.cmd
+++ Install.cmd
@@ -1,5 +1,5 @@
 @echo off
 whoami /groups | find "12288" || echo "Run me as Admin" && pause && exit
 REM sc.exe create Renamer binPath= "C:\Windows\Renamer\Renamer.cmd" displayname= "Renamer" depend= tcpip start= delayed-auto
-sc.exe create Renamer binPath= "C:\Windows\Renamer\Renamer.cmd" displayname= "Renamer" depend= tcpip start= auto
+C:\Windows\System32\sc.exe create Renamer binPath= "C:\Windows\Renamer\Renamer.cmd" displayname= "Renamer" depend= tcpip start= auto
 powershell Set-ExecutionPolicy Bypass
